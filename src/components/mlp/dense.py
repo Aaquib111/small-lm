716f3c93cc005ae31b4465f4b399d30f34aac11e
Aaquib Syed <aaquibahm@gmail.com>
2026-05-02 14:19:26 -0400

    Standard MLP

---
+++ src/components/mlp/dense.py
@@ -0,0 +1,20 @@
+import torch
+import torch.nn as nn
+from jaxtyping import Float
+from torch import Tensor
+
+
+class MLP(nn.Module):
+    """Standard dense multi-layer perceptron"""
+
+    def __init__(self, d_model: int, d_hidden: int):
+        super().__init__()
+        self.W_in = nn.Parameter(torch.empty(d_hidden, d_model))
+        self.b_in = nn.Parameter(torch.empty(d_hidden))
+        self.relu = nn.ReLU()
+        self.W_out = nn.Parameter(torch.empty(d_model, d_hidden))
+
+    def forward(
+        self, x: Float[Tensor, "batch n_seq d_model"]
+    ) -> Float[Tensor, "batch n_seq d_model"]:
+        return self.relu(x @ self.W_in.T + self.b_in) @ self.W_out.T
