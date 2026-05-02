1fa9f3c6853c9e29b64acb458797040ccabba937
Aaquib Syed <aaquibahm@gmail.com>
2026-05-02 14:44:20 -0400

    Added initialization to MLP

--- src/components/mlp/dense.py
+++ src/components/mlp/dense.py
@@ -10,9 +10,13 @@ class MLP(nn.Module):
     def __init__(self, d_model: int, d_hidden: int):
         super().__init__()
         self.W_in = nn.Parameter(torch.empty(d_hidden, d_model))
-        self.b_in = nn.Parameter(torch.empty(d_hidden))
+        torch.nn.init.normal_(self.W_in, std=0.02)
+
+        self.b_in = nn.Parameter(torch.zeros(d_hidden))
         self.relu = nn.ReLU()
+
         self.W_out = nn.Parameter(torch.empty(d_model, d_hidden))
+        torch.nn.init.normal_(self.W_out, std=0.02)
 
     def forward(
         self, x: Float[Tensor, "batch n_seq d_model"]
